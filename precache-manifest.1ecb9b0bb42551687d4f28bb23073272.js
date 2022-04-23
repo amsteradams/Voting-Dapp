94d64dd6cd614c310fba7d55e6d1d315b44667c1
amsteradams <adam.k92@hotmail.fr>
2022-04-23 20:36:33 +0200

    Updates

---
+++ precache-manifest.1ecb9b0bb42551687d4f28bb23073272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abf609ef6c6bc74e8b51ccbaa543101",
+    "url": "/Voting-Dapp/index.html"
+  },
+  {
+    "revision": "97fb0965219c4b13da80",
+    "url": "/Voting-Dapp/static/css/main.45166a81.chunk.css"
+  },
+  {
+    "revision": "9709949fe90c51cf9c35",
+    "url": "/Voting-Dapp/static/js/2.eb2ba06c.chunk.js"
+  },
+  {
+    "revision": "97fb0965219c4b13da80",
+    "url": "/Voting-Dapp/static/js/main.487bd7ce.chunk.js"
+  },
+  {
+    "revision": "f7392e7c7d0b4866468e",
+    "url": "/Voting-Dapp/static/js/runtime-main.b41d44a1.js"
+  }
+]);
